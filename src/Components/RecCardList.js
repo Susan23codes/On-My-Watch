3d282bf580b13398230019b0c9e81dd5a23230ae
Susan <susan23codes@gmail.com>
2022-08-30 14:35:02 -0400

    committing before merge

--- src/Components/RecCardList.js
+++ src/Components/RecCardList.js
@@ -4,16 +4,34 @@ import axios from 'axios';
 import { Link } from 'react-router-dom'
 import { GiFilmProjector } from 'react-icons/gi';
 import Carousel from 'react-material-ui-carousel';
-import { red } from '@mui/material/colors';
-import { isVisible } from '@testing-library/user-event/dist/utils';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import CloseIcon from '@mui/icons-material/Close';
+import { CardActionArea } from '@mui/material';
 
 
+const style = {
+    // position: 'absolute',
+    marginTop: '400px',
+    marginLeft: '400px',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: '#d8e0e6',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 
 export default function RecCardList(props) {
     const { isLoggedIn, token, navigate, username } = props
 
     const [recommendationList, setRecommendationList] = useState(null)
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
 
 
@@ -37,21 +55,78 @@ export default function RecCardList(props) {
                 {isLoggedIn &&
                     <>
 
-                        <div className='homepage-sidebar'>
+                        {/* <div className='homepage-sidebar'>
                             <ul className='my-stuff'></ul>
                             <li><GiFilmProjector /><Link to={"/mywatchlist"} style={{ textDecoration: 'none', color: 'white' }}> My Watchlist</Link></li><br />
                             <li><GiFilmProjector /><Link to={"/following"} style={{ textDecoration: 'none', color: 'white' }}> Following</Link></li><br />
-                            <li><GiFilmProjector /><Link to={"/search"} style={{ textDecoration: 'none', color: 'white' }}> Search</Link></li><br />
+                            <li><a href="#" onClick={handleOpen} style={{ textDecoration: 'none', color: 'white' }}><GiFilmProjector /> Sentiment Analysis Color Key</a></li><br />
                             <li><GiFilmProjector /><Link to={"/watched"} style={{ textDecoration: 'none', color: 'white' }}> What I've Watched</Link></li><br />
                             <li><GiFilmProjector /><Link to={"/new"} style={{ textDecoration: 'none', color: 'white' }}> Make a New Recommendation</Link></li>
+                        </div> */}
+                        <div className='homepage-sidebar'>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <GiFilmProjector style={{marginTop:'20px'}}/>
+                                <GiFilmProjector style={{marginTop:'23px'}}/>
+                                <GiFilmProjector style={{marginTop:'24px'}}/>
+                                <GiFilmProjector style={{marginTop:'48px'}}/>
+                                <GiFilmProjector style={{marginTop:'49px'}}/>
+                            </div >
+                            <div style={{ display: 'flex', flexDirection: 'column', marginLeft:'px', }}>
+                                <Link to={"/mywatchlist"} style={{ textDecoration: 'none', color: 'white', marginBottom:'20px', marginTop:'20px' }}> My Watchlist</Link>
+                                <Link to={"/following"} style={{ textDecoration: 'none', color: 'white', marginBottom:'20px' }}> Following</Link>
+                                <a href="#" onClick={handleOpen} style={{ textDecoration: 'none', color: 'white', marginBottom:'20px' }}> Sentiment Analysis Color Key</a>
+                                <Link to={"/watched"} style={{ textDecoration: 'none', color: 'white', marginBottom:'20px' }}> What I've Watched</Link>
+                                <Link to={"/new"} style={{ textDecoration: 'none', color: 'white', marginBottom:'20px' }}> Make a New Recommendation</Link>
+                            </div>
                         </div>
                     </>
                 }
 
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-color-key"
+                    aria-describedby="modal-emotion-color-key"
+                >
+                    <Box sx={style}>
+                        <div className='close-color-key'>
+                            <CardActionArea style={{ width: '20px', height: '20px' }}>
+                                <CloseIcon style={{ height: '20px', width: '20px' }} onClick={handleClose} />
+                            </CardActionArea>
+                        </div>
+                        <div className='color-key' style={{ textAlign: 'center' }}>
+                            <div style={{ fontWeight: 'bold', marginBottom: '20px', textAlign: 'center', fontSize: '30px' }}>
+                                Color Key:
+                            </div>
+                            <div className='emotion-names' style={{ backgroundColor: '#A9DEF9' }} >
+                                Sadness
+                            </div>
+                            <div className='emotion-names' style={{ backgroundColor: '#FFFF99' }}>
+                                Joy
+                            </div>
+                            <div className='emotion-names' style={{ backgroundColor: '#E4C1F9' }}>
+                                Fear
+                            </div>
+                            <div className='emotion-names' style={{ backgroundColor: '#EDE7B1' }}>
+                                Disgust
+                            </div>
+                            <div className='emotion-names' style={{ backgroundColor: '#FBC4A3' }}>
+                                Surprise
+                            </div>
+                            <div className='emotion-names' style={{ backgroundColor: '#FF4137' }}>
+                                Anger
+                            </div>
+                            {/* <div className='emotion-names' style={{ backgroundColor: '#FF4137' }}>
+                                Neutral
+                            </div> */}
+                        </div>
+                    </Box>
+                </Modal>
+
 
                 <div className='homepage-cards-and-welcome'>
                     {isLoggedIn &&
-                        <p style={{ fontSize: 25 }}>Welcome, {username}!  Check out these latest recommendations!</p>
+                        <p style={{ fontSize: 25 }}>Welcome, {username}!  Check out these latest recommendations...</p>
                     }
 
                     <div className='card' style={{ height: '400px' }}>
